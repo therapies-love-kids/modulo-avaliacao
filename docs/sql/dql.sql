70d1455156093d647373c61595b5a5d83321c61d
Matheus <matheusbrenner23052004@gmail.com>
2025-01-23 12:06:15 +0000

    MOD: Queries de consultas do arquivo "testes_respostas.sql" foram passadas para o arquivo "dql.sql".

--- docs/sql/dql.sql
+++ docs/sql/dql.sql
@@ -1,60 +1,95 @@
--- 1. Listar todas as empresas
-SELECT * FROM EMPRESA;
+--1 Como obter os dados do paciente associado a um contrato específico?
+SELECT p.* FROM 
+    contrato c
+JOIN 
+    paciente p ON c.paciente_codigo = p.codigo WHERE c.id = '<id_do_contrato>';
+    
+--2 Qual o responsável associado a um determinado paciente? 
+SELECT 
+    r.*
+FROM 
+    vinculo v
+JOIN 
+    responsavel r ON v.responsavel_codigo = r.codigo
+WHERE 
+    v.paciente_codigo = '<codigo_do_paciente>'; 
 
--- 2. Buscar convênios ativos
-SELECT * FROM CONVENIO WHERE ativo = TRUE;
+--3 Como acessar as avaliações de um paciente específico?
+SELECT * FROM avaliacao WHERE paciente_codigo = '<codigo_do_paciente>';
 
--- 3. Listar colaboradores ativos de uma empresa específica
-SELECT * FROM COLABORADOR
-WHERE ativo = TRUE AND empresa_unidade_codigo = '001';
+--4 Qual o convênio utilizado por um paciente com base em seu código?
+SELECT convenio_id FROM paciente WHERE codigo = '<codigo_do_paciente>';
 
--- 4. Listar pacientes vinculados a um convênio específico
-SELECT * FROM PACIENTE
-WHERE convenio_id = 1;
+--5 Como listar todos os documentos relacionados a contratos de um determinado paciente?
+SELECT d.* FROM documento d
+JOIN paciente p ON d.paciente_codigo = p.codigo where p.codigo = '<codigo_do_paciente>';
 
--- 5. Buscar responsáveis com estado civil específico
-SELECT * FROM RESPONSAVEL
-WHERE estado_civil = 'Solteira';
+--6 Como encontrar o responsável de um paciente por meio do vínculo de um contrato?
+SELECT r.* FROM vinculo v
+JOIN responsavel r ON v.responsavel_codigo = r.codigo
+WHERE v.paciente_codigo = '<codigo_do_paciente>';
 
--- 6. Listar vínculos de um paciente específico
-SELECT * FROM VINCULO
-WHERE paciente_codigo = 'PAC001';
+--7 Quais colaboradores participaram de agendamentos com um paciente específico?
+SELECT c.* FROM colaborador c
+JOIN avaliacao a ON c.codigo = a.colaborador_codigo
+WHERE a.paciente_codigo = '<codigo_do_paciente>';
 
--- 7. Consultar pagamentos realizados por um responsável
-SELECT * FROM PAGAMENTO
-WHERE responsavel_codigo = 'RESP001';
+--8 Como obter todos os pagamentos associados a um determinado paciente, considerando os responsáveis vinculados?
+SELECT p.* FROM pagamento p
+JOIN vinculo v ON p.responsavel_codigo = v.responsavel_codigo
+WHERE v.paciente_codigo = '<codigo_do_paciente>';
 
--- 8. Buscar documentos ativos de um paciente
-SELECT * FROM DOCUMENTO
-WHERE ativo = TRUE AND paciente_codigo = 'PAC001';
+--9 Quem foi o colaborador especialista em um determinado agendamento?
+SELECT c.* FROM colaborador c
+JOIN avaliacao a ON c.codigo = a.colaborador_codigo
+WHERE a.id = '<id_do_agendamento>';
 
--- 9. Listar contratos de um responsável com seus pacientes
-SELECT * FROM CONTRATO
-WHERE responsavel_codigo = 'RESP001';
+--10 Quantas avaliações já foram realizadas para um paciente específico?
+SELECT COUNT(*) FROM avaliacao WHERE paciente_codigo = '<codigo_do_paciente>';
 
--- 10. Consultar agendamentos em uma data específica
-SELECT * FROM AGENDAMENTO
-WHERE data_hora_inicio::DATE = '2025-01-22';
+--11 Qual o status atual de um agendamento baseado no paciente e no colaborador?
+SELECT a.* FROM agendamento a
+JOIN colaborador c ON a.recepcionista_colaborador_codigo = c.codigo
+WHERE a.paciente_codigo = '<codigo_do_paciente>' AND c.codigo = '<codigo_do_colaborador>';
 
--- 11. Buscar contratos de um paciente
-SELECT * FROM CONTRATO
-WHERE paciente_codigo = 'PAC001';
+--12 Como identificar todos os colaboradores de uma unidade específica de uma empresa?
+SELECT c.* FROM colaborador c
+JOIN empresa u ON c.empresa_unidade_codigo = u.unidade_codigo
+WHERE u.unidade_codigo = '<codigo_da_unidade/empresa>';
 
--- 12. Contar o número de pacientes por unidade de empresa
-SELECT empresa_unidade_codigo, COUNT(*) AS total_pacientes
-FROM PACIENTE
-GROUP BY empresa_unidade_codigo;
+--13 Quais agendamentos foram conduzidos por colaboradores que atuam em uma unidade específica de uma empresa?
+SELECT a.* FROM agendamento a
+JOIN colaborador c ON a.recepcionista_colaborador_codigo = c.codigo
+JOIN empresa u ON c.empresa_unidade_codigo = u.unidade_codigo
+WHERE u.unidade_codigo = '<codigo_da_unidade/empresa>';
 
--- 13. Verificar agendamentos de um especialista em uma sala específica
-SELECT * FROM AGENDAMENTO
-WHERE especialista_colaborador_codigo = 'COL001' AND sala = 101;
+--14 Como verificar os dados da empresa associada a um agendamento especificado?
+SELECT e.* FROM agendamento a
+JOIN colaborador c ON a.recepcionista_colaborador_codigo = c.codigo
+JOIN empresa e ON c.empresa_unidade_codigo = e.unidade_codigo
+WHERE a.id = '<id_do_agendamento>';
 
--- 14. Listar responsáveis com mais de um vínculo
-SELECT responsavel_codigo, COUNT(*) AS total_vinculos
-FROM VINCULO
-GROUP BY responsavel_codigo
-HAVING COUNT(*) > 1;
+--15 Quais contratos estão associados a uma determinada cidade ou bairro?
+SELECT * FROM contrato WHERE
+cidade = '<Cidade>' AND bairro = '<Bairro>';
 
--- 15. Buscar colaboradores por especialidade
-SELECT * FROM COLABORADOR
-WHERE especialidade = 'Postura';
+--16 Qual o endereço completo dos responsáveis de um determinado paciente?
+SELECT 
+    r.nome AS responsavel_nome,
+    r.cpf AS responsavel_cpf,
+    c.logradouro,
+    c.numero,
+    c.complemento,
+    c.bairro,
+    c.cidade,
+    c.uf,
+    c.cep
+FROM 
+    vinculo v JOIN responsavel r 
+ON 
+    v.responsavel_codigo = r.codigo JOIN contrato c
+ON 
+    c.responsavel_codigo = r.codigo
+WHERE 
+    v.paciente_codigo = '<codigo_do_paciente>'
+    AND c.ativo = TRUE;
