5bcc363407f9c4b2254b128f4e6fb1fa1a21b310
Matheus <matheusbrenner23052004@gmail.com>
2025-02-18 11:40:45 +0000

    MOD

--- src/telaprincipal.tsx
+++ src/telaprincipal.tsx
@@ -3,34 +3,34 @@ import Calendar from "react-calendar";
 
 export default function telaprincipal() {
   return (
-    <div className="h-screen w-screen">
-      <div className="w-screen h-[10%] flex items-center p-2">
-        <div className="w-[5%] h-full flex items-center justify-center rounded-full shadow-xl">
-          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content">
-            <label
-              htmlFor="my-drawer"
-              className="btn btn-primary drawer-button"
-            >
-              Open drawer
-            </label>
-          </div>
-          <div className="drawer-side">
-            <label
-              htmlFor="my-drawer"
-              aria-label="close sidebar"
-              className="drawer-overlay"
-            ></label>
-            <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-              <li>
-                <a>Sidebar Item 1</a>
-              </li>
-              <li>
-                <a>Sidebar Item 2</a>
-              </li>
-            </ul>
-          </div>
+    <div className="h-screen w-screen relative">
+      <div className="absolute z-10 w-screen h-screen">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          <label
+            htmlFor="my-drawer"
+            className="btn btn-primary drawer-button absolute top-4 right-4"
+          >
+            Open drawer
+          </label>
         </div>
+        <div className="drawer-side absolute top-0 right-0 w-80 p-4 z-20">
+          <label
+            htmlFor="my-drawer"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu bg-base-200 text-base-content min-h-full">
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="w-screen h-[10%] flex items-center p-2">
         <div className="w-[95%] h-full flex items-center justify-center">
           <input
             type="text"
@@ -90,3 +90,4 @@ export default function telaprincipal() {
     </div>
   );
 }
+
