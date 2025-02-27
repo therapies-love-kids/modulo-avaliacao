8f5554440c4ecb9e24649461a1428fdabccf5d13
MathiasBelmont <matheusbrenner23052004@gmail.com>
2025-02-27 12:03:57 +0000

    MOD: Instalação do axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import axios from "axios"; 
 
 function Login() {
   const [username, setUsername] = useState("");
